599c76f6ea0c7f0aa11c6e6bafeb84b3eea1ffad
Daniel Moss <dmoss@stratoswp.com>
2017-11-24 22:36:50 -0500

    small change

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
-$("#category-button").on("click", function(){
+$("#category-button").on("click", function () {
     let categories = ["COUNTRY", "ROCK", "RAP", "HIP HOP", "R&B", "ALTERNATIVE", "GRUNGE", "BOY BANDS"]
     let category = categories[Math.floor(Math.random() * categories.length)];
-    console.log (category);
+    console.log(category);
     let categoryDiv = $("<p>");
 
     categoryDiv.text("Category is: " + category + ".  Good Luck Shitass!");
@@ -12,55 +11,55 @@ $("#category-button").on("click", function(){
 
 
 
-$("#submit-button").on("click", function() {
+$("#submit-button").on("click", function () {
     let songName = $("#song-search").val();
     var queryURL = "https://itunes.apple.com/search?term=" + songName + "&limit=2";
-      
-      console.log(songName);
+
+    console.log(songName);
     $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      .done(function(response) {
-          // Parse results into JSON because they are returned as a string, then get the preview url
-          let newResponse = JSON.parse(response);
-          let preview = newResponse.results[0].previewUrl;
-          let artist = newResponse.results[0].artistName;
-          let track = newResponse.results[0].trackName;
-          let albumCover = newResponse.results[0].artworkUrl100;
+            url: queryURL,
+            method: "GET"
+        })
+        .done(function (response) {
+            // Parse results into JSON because they are returned as a string, then get the preview url
+            let newResponse = JSON.parse(response);
+            let preview = newResponse.results[0].previewUrl;
+            let artist = newResponse.results[0].artistName;
+            let track = newResponse.results[0].trackName;
+            let albumCover = newResponse.results[0].artworkUrl100;
 
-          console.log(response);
-          console.log(newResponse); 
-          console.log(preview); 
+            console.log(response);
+            console.log(newResponse);
+            console.log(preview);
 
-      //   Create <img> element and add album cover             
-      let artwork = $("<img>");
-      artwork.attr("src", albumCover);
+            //   Create <img> element and add album cover             
+            let artwork = $("<img>");
+            artwork.attr("src", albumCover);
 
-      // Create div for artist and song title
-      let artistNameHolder = $("<p>");
-      artistNameHolder.text(artist);
+            // Create div for artist and song title
+            let artistNameHolder = $("<p>");
+            artistNameHolder.text(artist);
 
-      let songNameHolder = $("<p>");
-      songNameHolder.text(track);
-      /**
-      * Build out HTML Audio Player
-      */
-      let audioPlayer = $("<audio>");
-      audioPlayer.addClass("myAudioPlayer");
-      audioPlayer.attr("src", preview);
+            let songNameHolder = $("<p>");
+            songNameHolder.text(track);
+            /**
+             * Build out HTML Audio Player
+             */
+            let audioPlayer = $("<audio>");
+            audioPlayer.addClass("myAudioPlayer");
+            audioPlayer.attr("src", preview);
 
-      // Adds the audio player controls to the HTML5 Audio Element
-      audioPlayer.attr("controls", "controls");
+            // Adds the audio player controls to the HTML5 Audio Element
+            audioPlayer.attr("controls", "controls");
 
-      // This can be changed to true to autoplay the track
-      audioPlayer.attr("autoplay", true);
+            // This can be changed to true to autoplay the track
+            audioPlayer.attr("autoplay", true);
 
-      // Add audio player, album cover and song and artist name to the result box
+            // Add audio player, album cover and song and artist name to the result box
 
-       $("#audio-player").append(audioPlayer);
-       $("#album-cover").append(artwork);
-       $("#song-info").append(artistNameHolder);
-       $("#song-info").append(songNameHolder);
-      });
-  });
+            $("#audio-player").append(audioPlayer);
+            $("#album-cover").append(artwork);
+            $("#song-info").append(artistNameHolder);
+            $("#song-info").append(songNameHolder);
+        });
+});
